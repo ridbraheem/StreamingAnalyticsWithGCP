b15875bd2e6df8b95141b9b3ad3470420d229d74
Ibraheem <ribraheem@astoncarter.com>
2021-10-25 09:17:36 -0400

    Added More Static Data

---
+++ GCP_CloudFunction_NodeJsApi/Staticdata.js
@@ -0,0 +1,137 @@
+exports.Act = ["NULL"
+            ,"Delete"
+            , "Create"
+            , "Modify"
+            , "View"
+            ]
+exports.Obj = ["NULL"
+            , "TroubleShooting Ticket"
+            , "Remedy Ticket"
+            , "Alert"
+            , "Message"
+            , "Notes"
+            , "Forms"
+            ]
+
+exports.Pro = [ "NULL",
+                {
+                "username": "Khalil232",
+                "team": "Tier-1 Technical Support",
+                "location": "Georgia"
+                },
+                {
+                "username": "AJones3",
+                "team": "Tier-1 Technical Support",
+                "location": "Utah"
+                },
+                {
+                "username": "Jharlow",
+                "team": "Tier-2 Technical Support",
+                "location": "Georgia"
+                },
+                {
+                "username": "Gclooney",
+                "team": "Tier-2 Technical Support",
+                "location": "Alabama"
+                },
+                {
+                "username": "AGraham",
+                "team": "Customer Service",
+                "location": "Utah"
+                },
+                {
+                "username": "MBall3",
+                "team": "Customer Service",
+                "location": "Alabama"
+                },
+                {
+                "username": "KQuinn",
+                "team": "Tier-1 Technical Support",
+                "location": "Alabama"
+                },
+                {
+                "username": "BJones45",
+                "team": "Tier-1 Technical Support",
+                "location": "Utah"
+                },
+                {
+                "username": "HPierce",
+                "team": "Tier-2 Technical Support",
+                "location": "Georgia"
+                },
+                {
+                "username": "BWilliams23",
+                "team": "Tier-2 Technical Support",
+                "location": "Alabama"
+                },
+                {
+                "username": "KWest12",
+                "team": "Customer Service",
+                "location": "Utah"
+                },
+                {
+                "username": "CRonaldo",
+                "team": "Customer Service",
+                "location": "Georgia"
+                },
+                {
+                "username": "AlexanderW",
+                "team": "Tier-1 Technical Support",
+                "location": "Georgia"
+                },
+                {
+                "username": "JunyaW",
+                "team": "Tier-1 Technical Support",
+                "location": "Utah"
+                },
+                {
+                "username": "SJobs",
+                "team": "Tier-2 Technical Support",
+                "location": "Utah"
+                },
+                {
+                "username": "ASanders",
+                "team": "Tier-2 Technical Support",
+                "location": "Alabama"
+                },
+                {
+                "username": "BEillish",
+                "team": "Customer Service",
+                "location": "Georgia"
+                },
+                {
+                "username": "OzumbaM",
+                "team": "Customer Service",
+                "location": "Alabama"
+                },
+                {
+                "username": "GarrettB",
+                "team": "Tier-1 Technical Support",
+                "location": "Alabama"
+                },
+                {
+                "username": "KimT",
+                "team": "Tier-1 Technical Support",
+                "location": "Utah"
+                },
+                {
+                "username": "FlorenceU",
+                "team": "Tier-2 Technical Support",
+                "location": "Georgia"
+                },
+                {
+                "username": "MedleyJ",
+                "team": "Tier-2 Technical Support",
+                "location": "Alabama"
+                },
+                {
+                "username": "LoladeB",
+                "team": "Customer Service",
+                "location": "Utah"
+                },
+                {
+                "username": "MaryJ45",
+                "team": "Customer Service",
+                "location": "Georgia"
+                }
+            ]
---
+++ GCP_CloudFunction_NodeJsApi/server.js
@@ -0,0 +1,43 @@
+// jshint esversion: 6
+const express = require("express");
+const _ = require('lodash');
+const {
+     Act
+    ,Obj
+    ,Pro
+} = require('./Staticdata') // Import Static data
+
+const app = express();
+
+var current_time = new Date();
+var hourago = new Date(current_time.getTime() - (1000*60*60));
+
+const randomDate = (start, end) => {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  }
+
+
+const CreateRandomJson = () => {
+    var randomJ = {
+        "activity" : _.sample(Act),
+        "object" : _.sample(Obj),
+        "profile" : _.sample(Pro),
+        "time" : randomDate(hourago,current_time)
+    }
+    return randomJ
+};
+
+const gHome = (req, res) => {
+    var randomJson = []
+    while(randomJson.length < 1000) {
+        data = CreateRandomJson()
+        randomJson.push(data)
+    }
+    res.json(randomJson);
+  }
+
+
+app.get('/', gHome);
+
+app.listen(process.env.PORT || 3000, function() {
+  });
