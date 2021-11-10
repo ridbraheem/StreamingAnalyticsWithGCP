6c016894d09dcebd06667183147968afca91cd86
Ridwanullah Ibraheem <44730057+ridbraheem@users.noreply.github.com>
2021-11-10 12:04:09 -0500

    Update sql_summary_query.sql

--- sql_summary_query.sql
+++ sql_summary_query.sql
@@ -15,7 +15,7 @@ FROM (
         eventTime
         ,FORMAT_DATETIME("%Y-%m-%d %H:%M:%S",DATETIME_TRUNC(DATETIME (eventTime), Hour)) as Hour
         ,activity
-        FROM `rapid-gadget-330102.emp_tool_tracking.raw_activity_table` 
+        FROM `PROJECT.DATASET.raw_activity_table` 
         WHERE DATE(processingTime) = CURRENT_DATE() LIMIT 100000
     ) r
     GROUP BY r.Hour,r.activity)
